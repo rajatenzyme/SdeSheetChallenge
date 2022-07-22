88dc5f0cfcd4d85fde5d30f279775e6e8fd72fbb
Rajat Mittal <rajatmittal2012@gmail.com>
2022-07-23 01:15:00 +0530

    day 4

---
+++ DAY4-ARRAY/TwoSum.cpp
@@ -0,0 +1,17 @@
+vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        unordered_map<int, int> mp;
+        for(int i=0;i<nums.size();i++){
+            int x = target - nums[i];
+            
+            if(mp.find(x)!=mp.end()){
+                
+                ans.push_back(mp[x]);
+                ans.push_back(i);
+            }
+            else{
+                mp[nums[i]] = i;
+            }
+        }
+        return ans;
+    }
